666fff33a28a20bc1d577150aa3502f1c08cad2e
Andrew Devlin and Jason  Brown <jason@>
2017-02-07 09:15:14 -0800

    established set and get methods for car properties

--- src/Car.php
+++ src/Car.php
@@ -4,13 +4,52 @@
         private $make_model;
         private $price;
         private $miles;
+        private $image;
 
-        function __construct($car_name, $car_price, $car_miles)
+        function __construct($car_name, $car_price, $car_miles, $car_image)
         {
             $this->make_model = $car_name;
             $this->price = $car_price;
             $this->miles = $car_miles;
+            $this->image = $car_image;
+        }
 
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function setPrice($new_price)
+        {
+            $this->price = $new_price;
+        }
+
+        function setMiles($new_miles)
+        {
+            $this->miles = $new_miles;
+        }
+
+        function setImage($new_image)
+        {
+            $this->image = $new_image;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getPrice()
+        {
+            return $this->price;
+        }
+        function getMiles()
+        {
+            return $this->miles;
+        }
+        function getImage()
+        {
+            return $this->image;
         }
     }
 ?>

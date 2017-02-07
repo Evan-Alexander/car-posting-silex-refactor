04d983733209ea51a4da009a7b139f1d9c6951ce
Andrew Devlin and Jason  Brown <andrew@>
2017-02-07 10:30:21 -0800

    create foreach loop in object to compare the instances with the user input

--- app/app.php
+++ app/app.php
@@ -1,18 +1,19 @@
 <?php
+
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Car.php";
+
     $app = new Silex\Application();
 
 
     $app->get("/", function() {
-        $porche = new Car("2014 Porsche 911", 114991, 7864);
-        $ford = new Car("2011 Ford F450", 55995, 14241);
-        $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
-        $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
-
-
-        $cars = array($porche, $ford, $lexus, $mercedes);
-
-
-
-
+        return "works"
     });
 ?>
+
+<!-- $output = "";
+foreach($cars as $car) {
+    $output = $output . "<div class='row'>
+        <div class='col-md-6'>
+            <img src= " . $ -->
--- src/Car.php
+++ src/Car.php
@@ -59,4 +59,20 @@
             }
         }
     }
+
+    $porche = new Car("2014 Porsche 911", 114991, 7864);
+    $ford = new Car("2011 Ford F450", 55995, 14241);
+    $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+
+    $cars = array($porche, $ford, $lexus, $mercedes);
+
+    $cars_matching_search =array();
+    foreach ($cars as $car)
+    {
+        if ($car->worthBuying($_GET["price"], $_GET["miles"]))
+        {
+            array_push($cars_matching_search, $car);
+        }
+    }
 ?>

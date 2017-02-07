60755ba3def31c74535c8797faeca3aefbda60c3
Andrew Devlin and Jason  Brown <andrew@>
2017-02-07 09:03:41 -0800

    create Car class and constructor

--- src/Car.php
+++ src/Car.php
@@ -5,6 +5,12 @@
         private $price;
         private $miles;
 
-        
+        function __construct($car_name, $car_price, $car_miles)
+        {
+            $this->make_model = $car_name;
+            $this->price = $car_price;
+            $this->miles = $car_miles;
+
+        }
     }
 ?>
